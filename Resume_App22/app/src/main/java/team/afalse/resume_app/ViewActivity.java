ebe29e409778d98980c9169268df631767230e6c
ppypenko <ppypenko@student.neumont.edu>
2017-03-08 06:26:40 -0700

    Added View Activity

---
+++ Resume_App22/app/src/main/java/team/afalse/resume_app/ViewActivity.java
@@ -0,0 +1,13 @@
+package team.afalse.resume_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+    }
+}
